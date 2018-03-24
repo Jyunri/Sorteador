eb818fc372f968dfd8b76fd6ee6a9b83040d2b5b
Jyunri <nekojimy@gmail.com>
2018-03-24 15:35:03 -0300

    update

--- app/src/main/java/br/com/cdf/sorteador/MainActivity.java
+++ app/src/main/java/br/com/cdf/sorteador/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.io.InputStream;
 import java.util.ArrayList;
